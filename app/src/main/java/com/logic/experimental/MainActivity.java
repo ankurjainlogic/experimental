6a42e2b68d6711d048d41fe2bfe6c3cf83339f71
user6 <nature.land12@gmail.com>
2017-01-04 11:11:18 +0530

    Initial commit

---
+++ app/src/main/java/com/logic/experimental/MainActivity.java
@@ -0,0 +1,27 @@
+package com.logic.experimental;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+Button btn1;
+    ImageView ivPickedPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1=(Button)findViewById(R.id.btn1);
+        ivPickedPhoto=(ImageView)findViewById(R.id.ivPickedPhoto);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //    selectImage();
+            }
+        });
+    }
+
+}
